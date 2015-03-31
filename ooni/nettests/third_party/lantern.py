331a44ddc761b966dc30c3694258970b96f1fe57
aagbsn <aagbsn@extc.org>
2015-03-31 10:35:51 +0000

    var name

--- ooni/nettests/third_party/lantern.py
+++ ooni/nettests/third_party/lantern.py
@@ -9,7 +9,7 @@ class LanternBootstrapProcessDirector(ProcessDirector):
     before timing out.
     """
     def __init__(self, d, finished=None, timeout=None, stdin=None):
-        super(LanternProcessDirector, self)._init_(self, d, finished, timeout, stdin)
+        super(LanternBootstrapProcessDirector, self)._init_(self, d, finished, timeout, stdin)
         self.bootstrapped = defer.Deferred()
 
     def outReceived(self, data):
